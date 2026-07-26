e45f10901bd4f00f27300242cfca37219444a060
Cat2333 <gc169@duke.edu>
2026-07-26 15:39:57 -0400

    Step 4: is_cell_empty

--- model.py
+++ model.py
@@ -38,8 +38,14 @@ def print_board(board):
     for row in board:
         print(" ".join(symbols[int(cell)] for cell in row))
 
-# Step 4 - is_cell_empty (not yet solved)
-# TODO: implement
+# Step 4 - is_cell_empty
+import numpy as np
+
+def is_cell_empty(board, row, col):
+    """Return True if board[row, col] is empty (0), else False."""
+    # TODO: check whether the cell at (row, col) is empty
+
+    return board[row][col] == 0
 
 # Step 5 - place_move (not yet solved)
 # TODO: implement
