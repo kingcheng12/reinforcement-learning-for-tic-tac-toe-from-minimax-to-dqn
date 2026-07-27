513bf99d538c804653f53638e1eb8109c144565b
Cat2333 <gc169@duke.edu>
2026-07-27 16:14:28 -0400

    Step 15: switch_player

--- model.py
+++ model.py
@@ -189,8 +189,12 @@ def get_current_player(board):
     
     return 1 if X_count == O_count else -1
 
-# Step 15 - switch_player (not yet solved)
-# TODO: implement
+# Step 15 - switch_player
+def switch_player(player):
+    """Return the opponent of `player` (1 <-> -1)."""
+    # TODO: return the opposite player given 1 for X and -1 for O.
+
+    return -player
 
 # Step 16 - play_hardcoded_game (not yet solved)
 # TODO: implement
