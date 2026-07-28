bd9c677f65cabfac8ff1ef54bb8fc878e2cbb578
Cat2333 <gc169@duke.edu>
2026-07-27 20:06:39 -0400

    Step 27: minimax_best_move

--- model.py
+++ model.py
@@ -432,8 +432,14 @@ def minimax_max_min_step(board, player):
             
     return best_score, best_move
 
-# Step 27 - minimax_best_move (not yet solved)
-# TODO: implement
+# Step 27 - minimax_best_move
+def minimax_best_move(board, player):
+    """Return the optimal (row, col) move for `player` via minimax."""
+    # TODO: use the minimax max/min step to pick the best legal move for player
+
+    _, move = minimax_max_min_step(board, player)
+
+    return move
 
 # Step 28 - minimax_alpha_beta (not yet solved)
 # TODO: implement
